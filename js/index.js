e994605e28474eae8f68b4f276c401d129fdc48a
Mario Johar <mjohar30@gmail.com>
2018-12-17 19:03:26 -0600

    Pagina funcional sin diseño

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
-var contenido = document.querySelector('#contenido')
+var nombre = document.querySelector('#nombre')
+var direccion = document.querySelector('#direccion')
+var rating = document.querySelector('#rating')
+var datos = document.querySelector('#datos')
 
 fetch(' https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json')
     .then(data => data.json())
     .then(data => {
-
-    console.log(data[0]);
-    contenido.innerHTML = 'data'
+    var random = data[Math.floor(Math.random()*data.length)]
+    console.log(random)
+    
+    nombre.innerHTML = `${random.name}`
+    
+    rating.innerHTML = ` <small class="text-muted">Rating: ${random.rating} </small> `
+    
+    direccion.innerHTML = `<p class="card-text"> La dirección del lugar es: </p>
+    <p class="card-text">  ${random.address.street} </p>
+    ${random.address.city} , ${random.address.state}
+    `
+    datos.innerHTML = ` <p class="card-text"> Para reservar, favor de comunicarse por estos medios: </p>
+    <p class="card-text">  Teléfono: ${random.contact.phone} </p>
+    <p class="card-text"> Sitio web: <a href= " ${random.contact.site}"> ${random.contact.site} </a> </p>
+    <p class="card-text"> Email: ${random.contact.email} </p>
+    `
 
   }).catch(err => {
     // Do something for an error here
