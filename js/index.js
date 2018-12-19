fc213518ac3753c18ae3467efadabac4452c755b
mjohar30 <40815898+mjohar30@users.noreply.github.com>
2018-12-19 00:12:36 -0600

    Final

--- js/index.js
+++ js/index.js
@@ -2,11 +2,10 @@ var nombre = document.querySelector('#nombre')
 var direccion = document.querySelector('#direccion')
 var rating = document.querySelector('#rating')
 var datos = document.querySelector('#datos')
-<<<<<<< HEAD
+
 var latitud, leng
-=======
+
 var lat, leng
->>>>>>> a859bf2d1b90a85c41a57d47a6ee3d497b064260
 
     fetch(' //s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json')
     .then(data => data.json())
@@ -27,15 +26,15 @@ var lat, leng
         <p class="card-text"> Sitio web: <a href= " ${random.contact.site}"> ${random.contact.site} </a> </p>
         <p class="card-text"> Email: ${random.contact.email} </p>
         `
-<<<<<<< HEAD
+
         latitud = random.address.location.lat
         leng = random.address.location.lng
         console.log(latitud)
-=======
+
         lat = random.address.location.lat
         leng = random.address.location.lng
         console.log(lat)
->>>>>>> a859bf2d1b90a85c41a57d47a6ee3d497b064260
+
         console.log(leng)
 }).catch(err => {
     // Do something for an error here
@@ -44,17 +43,16 @@ var lat, leng
 function initMap() {
     var map;
      map = new google.maps.Map(document.getElementById('map'), {    
-<<<<<<< HEAD
+
         center: {lat: latitud, lng: leng},
         zoom: 18
       });
       var marcador = {lat: latitud, lng: leng}
-=======
+
         center: {lat: lat, lng: leng},
         zoom: 18
       });
       var marcador = {lat: lat, lng: leng}
->>>>>>> a859bf2d1b90a85c41a57d47a6ee3d497b064260
       marker = new google.maps.Marker({position: marcador, map: map});
     }
 
